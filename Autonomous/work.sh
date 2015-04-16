f9686c7ef8fff446b57b97e792080798ca5bd532
JinfengChen <jinfeng7chen@gmail.com>
2015-04-16 16:51:22 -0700

    c80, family cluster with 80% identify

--- Autonomous/work.sh
+++ Autonomous/work.sh
@@ -38,6 +38,8 @@ perl match_predicted-peps_glsearch.pl /rhome/cjinfeng/BigData/00.RD/Mosquito_TE/
 echo "Cluster nonredundant hit sequence"
 python make_cdhit-prot_sh.py /rhome/cjinfeng/BigData/00.RD/Mosquito_TE/Autonomous/Target/nonredundant_pep_union-hits.fa
 qsub cd-hit_nonredundant_pep_union-hits-90.sh
+python make_multi_seq_sum.py Target/nonredundant_pep_union-hits_c90.clstr
+python make_multi_seq_sum.py Target/nonredundant_pep_union-hits_c80.clstr
 
 echo "Generate sequence files of each clusterwith >= 2 seqs and 1 only"
 perl make_multi_seq.pl Target/nonredundant_pep_union-10kb.fa Target/nonredundant_pep_union-hits_c90.clstr Target/nonredundant_pep_union-10kb_c90_multi 2
@@ -45,6 +47,8 @@ perl make_multi_seq.pl Target/nonredundant_pep_union-10kb.fa Target/nonredundant
 perl make_multi_seq.pl Target/nonredundant_pep_union-hits.fa Target/nonredundant_pep_union-hits_c90.clstr Target/nonredundant_pep_union-hits_c90_multi 2
 perl make_multi_seq.pl Target/nonredundant_pep_union-hits.fa Target/nonredundant_pep_union-hits_c90.clstr Target/nonredundant_pep_union-hits_c90_single 1
 
+perl make_multi_seq.pl Target/nonredundant_pep_union-10kb.fa Target/nonredundant_pep_union-hits_c80.clstr Target/nonredundant_pep_union-10kb_c80_multi 2
+perl make_multi_seq.pl Target/nonredundant_pep_union-10kb.fa Target/nonredundant_pep_union-hits_c80.clstr Target/nonredundant_pep_union-10kb_c80_single 1
 
 ##Can stop here for MULE project for Kun, just use activeTE to find TSD and TIR
 
@@ -61,15 +65,21 @@ python summarize_protein_matches.py Target/nonredundant_pep_union-hits_match-inf
 ##activeTE for autonomous cluster
 echo "Trim 10 kb flanks down to 5kb (will use 10kb if need, i.e. flanks are too conserved by aTE analysis)"
 python trim_flanks_directory.py Target/nonredundant_pep_union-10kb_c90_multi/ 5000
+python trim_flanks_directory.py Target/nonredundant_pep_union-10kb_c80_multi/ 5000
 
 echo "Run multiple sequence alignments"
 python make_mafft_pep-cluster_split_one_sh.py Target/nonredundant_pep_union-10kb_c90_multi/ '*trimmed-5000.fa' aedes
+python make_mafft_pep-cluster_split_one_sh.py Target/nonredundant_pep_union-10kb_c80_multi/ '*trimmed-5000.fa' aedes
+qsub -q js mafft_aedes.sh
 
 echo "Analyzing MSA files with activeTE"
 python make_activeTE-pep-msa-one.py Target/nonredundant_pep_union-10kb_c90_multi/ '*.msa' c90
+python make_activeTE-pep-msa-one.py Target/nonredundant_pep_union-10kb_c80_multi/ '*.msa' c80
+qsub aTE-pep_c80.sh
 
 echo "Initial summarizing of aTE"
 python list_aTE_good_bad_mixed_pep.py Target/nonredundant_pep_union-10kb_c90_multi/aTE Target/nonredundant_pep_union-10kb_c90_multi/aTE_sum aedes_pep
+python list_aTE_good_bad_mixed_pep.py Target/nonredundant_pep_union-10kb_c80_multi/aTE Target/nonredundant_pep_union-10kb_c80_multi/aTE_sum aedes_pep
 
 echo "Recombine results of split groups"
 python recombine_aTE-out3.py Target/nonredundant_pep_union-10kb_c90_multi/aTE_sumaedes_pep.good Target/nonredundant_pep_union-10kb_c90_multi/aTE/ Target/nonredundant_pep_union-10kb_c90_multi/aTE_recombine aedes_pep
