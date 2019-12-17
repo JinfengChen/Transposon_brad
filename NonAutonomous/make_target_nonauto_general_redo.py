a86bc3015342408a280131b55f3b441fc6419772
JinfengChen <jinfeng7chen@gmail.com>
2019-12-16 17:02:48 -0800

    some addintions

--- NonAutonomous/make_target_nonauto_general_redo.py
+++ NonAutonomous/make_target_nonauto_general_redo.py
@@ -6,7 +6,6 @@ import re
 import fnmatch
 import os.path
 import subprocess as subp
-import fastaIO
 import glob
 import fnmatch
 
@@ -27,25 +26,23 @@ if len(args) != 4 or sys.argv[1] == '-h' or sys.argv[1] == '-help' or sys.argv[1
 files = os.listdir(sys.argv[1])
 
 top = '''#!/bin/bash
-#PBS -l nodes=1:ppn=16,mem=16g
+#PBS -l nodes=1:ppn=1,mem=200g
 #PBS -l walltime=100:00:00
-module load stajichlab
-module load stajichlab-python
 module load mafft
-module load FastTree
 module load treebest
-module load target
 
 cd $PBS_O_WORKDIR
-CPU1=1
-CPU2=16
+export PATHONPATH=$PATHONPATH:/rhome/cjinfeng/BigData/software/pythonlib/lib/python2.7/site-packages:/rhome/cjinfeng/BigData/software/Target/2.0/
+export PATH=$PATH:/rhome/cjinfeng/BigData/software/fasttree/
+CPU1=$PBS_NP
+CPU2=1
 mode=mi
 
-/rhome/cjinfeng/software/tools/Target/2.0/target.py -q '''
+/rhome/cjinfeng/BigData/software/Target/2.0/target.py -q '''
 
 middle = ''' -t nucl -o '''
 
-bottom = ''' -i $mode -P $CPU1 -C $CPU2 -b_a 12000 -DB -b_d 10 -E -W 5 -f 1.2 -a flanks -p_M 0.40 -p_n 12000 -p_d 6000 -p_f 300 '''
+bottom = ''' -i $mode -P $CPU1 -C $CPU2 -b_a 12000 -DB -b_d 10 -E -W 5 -f 1.2 -a flanks -p_M 0.40 -p_n 12000 -p_d 6000 -p_f 300 -S 'MSA' '''
 
 if not os.path.exists(sys.argv[2]):
     os.mkdir(sys.argv[2])
